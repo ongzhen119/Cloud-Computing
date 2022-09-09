1b150b43d752f1db37bc33e38f96653113c34da4
ongzhen119 <ongzc-pm19@student.tarc.edu.my>
2022-09-09 13:50:28 +0800

    updated code

--- EmpApp.py
+++ EmpApp.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from pymysql import connections
 import os
 import boto3
 from config import *
-
+session = boto3.Session(
+    aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
+    aws_secret_access_key=AWS_SERVER_SECRET_KEY,
+    aws_session_token=AWS_SESSION_TOKEN
+)
 app = Flask(__name__)
 
 bucket = custombucket
@@ -30,6 +34,12 @@ def home():
 def about():
     return render_template('www.intellipaat.com')
 
+@app.route("/looz", methods=["POST"])
+def ShowReceivedData():
+    print(request.form)
+    print('hello?')
+    resp = jsonify(success=True)
+    return resp
 
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -53,12 +63,16 @@ def AddEmp():
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
+        s3 = session.resource('s3')
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
+
+            # Send to S3
             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+
+
+            bucket_location = session.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
 
             if s3_location is None:
@@ -80,6 +94,9 @@ def AddEmp():
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
+@app.route("/getemp", methods = ["GET"])
+def getEmp():
+    return render_template("GetEmp.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
